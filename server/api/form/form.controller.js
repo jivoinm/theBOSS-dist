0149c507f1c057252ad2f4ab03445ad5ff83121d
Mircea Jivoin <jivoinm@gmail.com>
2015-10-25 20:32:45 +0100

    fixed field option changes issue

--- server/api/form/form.controller.js
+++ server/api/form/form.controller.js
@@ -125,7 +125,9 @@ exports.updateField = function (req,res){
     if(req.user){
         var field = req.body;
         var target = req.params.target;
-        var query ={};
+        var query ={
+          _id: req.params.id
+        };
         query[target+'._id'] = req.params.targetId;
         var setValue= {};
         setValue.$set = {};
@@ -134,8 +136,6 @@ exports.updateField = function (req,res){
             query,
             setValue,
            function(err,updateCount){
-                console.log('error', err);
-                console.log('updated', updateCount);
                 if(err) return res.json(400,err);
                 Form.findById(req.params.id, function (err, form) {
                     return res.send(form);
