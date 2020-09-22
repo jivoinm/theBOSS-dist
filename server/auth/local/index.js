ec7f974c1d845b583f2ec204132613e7a0df8ac6
Mirća Živojin <jivoinm@gmail.com>
2020-09-22 09:58:22 -0400

    init

--- server/auth/local/index.js
+++ server/auth/local/index.js
@@ -9,9 +9,9 @@ var router = express.Router();
 router.post('/', function(req, res, next) {
   passport.authenticate('local', function (err, user, info) {
     var error = err || info;
-    if (error) return res.json(401, error);
-    if (!user) return res.json(404, {message: 'Something went wrong, please try again.'});
-
+    //if (error) return res.json(401, error);
+    //if (!user) return res.json(404, {message: 'Something went wrong, please try again.'});
+    console.log(error);
     var token = auth.signToken(user._id, user.role);
     res.json({token: token, owner: user.owner});
   })(req, res, next)
